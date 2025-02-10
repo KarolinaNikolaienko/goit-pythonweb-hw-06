1f1ce4c145a59effdfe8b04f056e1bb7baee4926
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2025-02-10 03:49:55 +0100

    rewritten Grade table and func to add grades added to seed.py

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from sqlalchemy import select
 
@@ -38,30 +38,24 @@ def make_students(amount):
         session.add(student_obj)
     session.commit()
 
-def make_grades(students: [], subjects: []):
-    for student in students: # для кожного студента
-        for subject in subjects: # з усіх предметів
+def make_grades(students_list: [], subjects_list: []):
+    for student in students_list: # для кожного студента
+        for subject in subjects_list: # з усіх предметів
             for i in range(random.randint(10,20)): # додати по 10-20 оцінок
-                grade_date = fake.date_between_dates(datetime(2025, 1, 6), datetime.now())
-                grade_obj = Grade(grade=random.randint(1, 5), date=grade_date, student_id=student.id, subject_id=subject.id)
+                grade_date = fake.date_between_dates(date(2025, 1, 6), date.today())
+                grade_obj = Grade(grade=random.randint(0, 100), date=grade_date, student_id=student.id, subject_id=subject.id)
                 session.add(grade_obj)
     session.commit()
 
 if __name__ == "__main__":
-    # make_groups(GROUPS)
-    # make_teachers(AMOUNT_TEACHERS)
-    # make_subjects(SUBJECTS)
-    # make_students(AMOUNT_STUDENTS)
+    make_groups(GROUPS)
+    make_teachers(AMOUNT_TEACHERS)
+    make_subjects(SUBJECTS)
+    make_students(AMOUNT_STUDENTS)
 
-    q = session.execute(select(Student.id, Student.name, Student.group_id)).mappings().all()
-    print(q)
+    students = session.query(Student).all()
+    subjects = session.query(Subject).all()
+    make_grades(students, subjects) # помилка з id = null під час вставки у таблицю Grade
 
     session.close()
-    # students = []
-    # for i in range(30):
-    #     students.append(Student(name=fake.name(), group_id=random.randint(1,3)))
-    #
-    #
-    # session.bulk_insert_mappings(Student, students)
-    # session.commit()
-    # session.close()
+
