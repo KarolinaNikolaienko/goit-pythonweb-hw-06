b1c596249f6d1e8db96aeb5dd303d396f5b45039
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2025-02-10 02:28:51 +0100

    added functions to seed.py

--- models.py
+++ models.py
@@ -4,49 +4,48 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 class Base(DeclarativeBase):
     pass
 
-grades = Table(
-    "grades",
-    Base.metadata,
-    Column("student_id", Integer, ForeignKey("students.id", ondelete="CASCADE")),
-    Column("subject_id", Integer, ForeignKey("subjects.id", ondelete="CASCADE")),
-    Column("grade", Integer()),
-    Column("date", Date()),
-    PrimaryKeyConstraint("student_id", "subject_id"),
-)
-
-class Student(Base):
-    __tablename__ = "students"
-    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
-    group_id: Mapped[int] = mapped_column(Integer(), ForeignKey('groups.id'))
-    subjects: Mapped[list["Subject"]] = relationship(
-        secondary=grades, back_populates="students"
-    )
-
 class Group(Base):
     __tablename__ = "groups"
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     group_name: Mapped[str] = mapped_column(String(5), nullable=False, unique=True)
 
+    students: Mapped[list['Student']] = relationship('Student', back_populates='group')
+
+class Student(Base):
+    __tablename__ = "students"
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    group_id: Mapped[int] = mapped_column(Integer(), ForeignKey('groups.id'))
+    group: Mapped['Group'] = relationship('Group', back_populates="students")
+
+    grades: Mapped[list['Grade']] = relationship('Grade', back_populates="student")
+
 class Teacher(Base):
     __tablename__ = "teachers"
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
-    subjects:Mapped[list['Subject']] = relationship(back_populates='teacher')
+    subjects:Mapped[list['Subject']] = relationship('Subject', back_populates='teacher')
 
 class Subject(Base):
     __tablename__ = "subjects"
     id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     title: Mapped[str] = mapped_column(String(50), nullable=False)
-    teacher: Mapped['Teacher'] = relationship(back_populates='subjects')
-    students: Mapped[list["Student"]] = relationship(
-        secondary=grades, back_populates="subjects"
-    )
 
-# class Grade(Base):
-#     __tablename__ = "grades"
-#     student_id: Mapped[int] = mapped_column(Integer(), ForeignKey('students.id', ondelete="CASCADE"))
-#     subject_id: Mapped[int] = mapped_column(Integer(), ForeignKey('subjects.id', ondelete="CASCADE"))
-#     grade: Mapped[int] = mapped_column(Integer(), nullable=False)
-#     date: Mapped[Date] = mapped_column(Date(), nullable=False)
-#     PrimaryKeyConstraint("student_id", "subject_id")
+    teacher_id: Mapped[int] = mapped_column(Integer(), ForeignKey('teachers.id'))
+    teacher: Mapped['Teacher'] = relationship('Teacher', back_populates='subjects')
+
+    grades: Mapped[list['Grade']] = relationship('Grade', back_populates="subject")
+
+
+class Grade(Base):
+    __tablename__ = "grades"
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    grade: Mapped[int] = mapped_column(Integer(), nullable=False)
+    date: Mapped[Date] = mapped_column(Date(), nullable=False)
+
+    student_id: Mapped[int] = mapped_column(Integer(), ForeignKey('students.id'))
+    student: Mapped['Student'] = relationship('Student', back_populates='grades')
+
+    subject_id: Mapped[int] = mapped_column(Integer(), ForeignKey('subjects.id'))
+    subject: Mapped['Subject'] = relationship('Subject', back_populates='grades')
--- seed.py
+++ seed.py
@@ -1,18 +1,67 @@
+from datetime import datetime
+
+from sqlalchemy import select
+
 from connect import session
-from models import Student, Teacher, Subject, Grade, Group
+from models import Student, Teacher, Subject, Group, Grade
 from faker import Faker
 import random
 
 fake = Faker()
 
-if __name__ == "__main__":
-    groups = [Group(group_name="A-1"), Group(group_name="B-4"), Group(group_name="E-2")]
+GROUPS = ["A-1", "B-4", "E-2"]
+SUBJECTS = ["English", "Math", "Art", "History", "Physics", "Chemistry", "PE", "IT"]
+AMOUNT_TEACHERS = 5
+AMOUNT_STUDENTS = 30
 
-    students = []
-    for i in range(30):
-        students.append(Student(name=fake.name(), group_id=random.randint(1,3)))
-
-
-    session.bulk_insert_mappings(Student, students)
+def make_groups(groups_list: []):
+    for group in groups_list:
+        group_obj = Group(group_name=group)
+        session.add(group_obj)
     session.commit()
+
+def make_teachers(amount):
+    for i in range(amount):
+        teacher_obj = Teacher(name=fake.name())
+        session.add(teacher_obj)
+    session.commit()
+
+def make_subjects(subjects_list: []):
+    for subject in subjects_list:
+        subject_obj = Subject(title=subject, teacher_id=random.randint(1,AMOUNT_TEACHERS))
+        session.add(subject_obj)
+    session.commit()
+
+def make_students(amount):
+    for i in range(amount):
+        student_obj = Student(name=fake.name(), group_id=random.randint(1, len(GROUPS)))
+        session.add(student_obj)
+    session.commit()
+
+def make_grades(students: [], subjects: []):
+    for student in students: # для кожного студента
+        for subject in subjects: # з усіх предметів
+            for i in range(random.randint(10,20)): # додати по 10-20 оцінок
+                grade_date = fake.date_between_dates(datetime(2025, 1, 6), datetime.now())
+                grade_obj = Grade(grade=random.randint(1, 5), date=grade_date, student_id=student.id, subject_id=subject.id)
+                session.add(grade_obj)
+    session.commit()
+
+if __name__ == "__main__":
+    # make_groups(GROUPS)
+    # make_teachers(AMOUNT_TEACHERS)
+    # make_subjects(SUBJECTS)
+    # make_students(AMOUNT_STUDENTS)
+
+    q = session.execute(select(Student.id, Student.name, Student.group_id)).mappings().all()
+    print(q)
+
     session.close()
+    # students = []
+    # for i in range(30):
+    #     students.append(Student(name=fake.name(), group_id=random.randint(1,3)))
+    #
+    #
+    # session.bulk_insert_mappings(Student, students)
+    # session.commit()
+    # session.close()
